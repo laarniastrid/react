05d525326b1da0a2e31ad0466411272a3569df43
Astrid Sutton <astrid.sutton@wgu.edu>
2018-04-25 21:41:28 -0600

    working on implementing time travel

--- src/index.js
+++ src/index.js
@@ -61,12 +61,13 @@ class Game extends React.Component {
 			history: [{
 				squares: Array(9).fill(null),
 			}],
+			stepNumber: 0,
 			xIsNext: true,
 		}
 	}
 
 	handleClick(i) {
-		const history = this.state.history;
+		const history = this.state.history.slice(0, this.state.stepNumber + 1);
 		const current = history[history.length - 1];
 		const squares = current.squares.slice();
 		if(calculateWinner(squares) || squares[i]) {
@@ -81,9 +82,16 @@ class Game extends React.Component {
 		})
 	}
 
+	jumpTo(step) {
+		this.setState({
+			stepNumber: step,
+			xIsNext: (step % 2) === 0,
+		});
+	}
+
 	render() {
 		const history = this.state.history;
-		const current = history[history.length - 1];
+		const current = history[this.state.stepNumber];
 		const winner = calculateWinner(current.squares);
 
 		const moves = history.map((step, move) => {
@@ -91,7 +99,7 @@ class Game extends React.Component {
 				'Go to move #' + move :
 				'Go to game start';
 			return (
-				<li>
+				<li key={move}>
 					<button onClick={() => this.jumpTo(move)} >
 						{desc}
 					</button>
